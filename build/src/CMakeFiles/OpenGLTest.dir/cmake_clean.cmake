8bfe4cdbc2dc43788376eb75940545a76abd4812
Kilian Mandon <kilian@pop-os.localdomain>
2020-12-28 20:53:41 +0100

    First commit

---
+++ build/src/CMakeFiles/OpenGLTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLTest.dir/OpenGLTest.cpp.o"
+  "OpenGLTest"
+  "OpenGLTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/OpenGLTest.cpp
@@ -0,0 +1,64 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    std::cout << "User resized to " <<  width << "/" << height << std::endl;
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+GLFWwindow* initializeWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window==NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return NULL;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader( (GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return NULL;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    return window;
+}
+
+int main() {
+
+    GLFWwindow *window = initializeWindow();
+
+    if (!window) {
+        std::cout << "Error creating the window. " << std::endl;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
